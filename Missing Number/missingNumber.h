6b855cde51f10b2c6bc6cebdb3cc60a43b6f7f82
Hamza Mogni <hamzamogni5@gmail.com>
2022-01-13 02:06:27 +0100

    Missing Number

---
+++ Missing Number/missingNumber.h
@@ -0,0 +1,12 @@
+//
+// Created by hmogni on 2/1/21.
+//
+
+#ifndef STUFF_MISSINGNUMBER_H
+#define STUFF_MISSINGNUMBER_H
+
+#include <vector>
+
+int missingNumber(std::vector<int>& nums);
+
+#endif //STUFF_MISSINGNUMBER_H
