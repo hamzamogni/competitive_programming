d85b15f0b14aeab487935060e22c36e91bf3c2fd
Hamza Mogni <hamzamogni5@gmail.com>
2022-01-19 01:12:02 +0100

    clean up

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <vector>
 
-#include "palindrome_number/palindromeNumber.h"
-
 using namespace std;
 
-int main() {
-    isPalindrome(121345);
-return 0;
+int main()
+{
+  vector<int> a;
+  a.push_back(5);
+  a.push_back(50);
+
+  for (int i = 0; i < a.size(); i++)
+  {
+    cout << a[i] << endl;
+  }
+
+  return 0;
 }
