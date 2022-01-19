e7a3c15405b17a4531044cb5d639bbed0085874c
Hamza Mogni <hamzamogni5@gmail.com>
2022-01-19 02:20:19 +0100

    create cpp folder

---
+++ cpp/containsDuplicate/ContainsDuplicate.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <vector>
+#include <assert.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        cout << "hihihihihi";
+
+        return true;
+    }
+};
+
+int main()
+{
+    int a = 3;
+    cout << "hamza" << endl;
+    return 0;
+}
