d2593372edee323c3f4f68eafd0ad9bdbafb52b2
Hamza Mogni <hamzamogni5@gmail.com>
2022-01-19 02:36:44 +0100

    Contains Duplicate in C++

--- cpp/containsDuplicate/ContainsDuplicate.cpp
+++ cpp/containsDuplicate/ContainsDuplicate.cpp
@@ -1,6 +1,13 @@
+/***
+ * 217. Contains Duplicate
+ * 
+ * Given an integer array nums, return true if any value appears at least twice
+ * in the array, and return false if every element is distinct.
+ ***/
 
 #include <iostream>
 #include <vector>
+#include <set>
 #include <assert.h>
 
 using namespace std;
@@ -8,17 +15,32 @@ using namespace std;
 class Solution
 {
 public:
+    // Time: o(n)
+    // Space: o(n)
     bool containsDuplicate(vector<int> &nums)
     {
-        cout << "hihihihihi";
+        set<int> visited;
 
-        return true;
+        for (int nbr : nums)
+        {
+            if (visited.find(nbr) != visited.end())
+            {
+                return true;
+            }
+
+            visited.insert(nbr);
+        }
+
+        return false;
     }
 };
 
 int main()
 {
-    int a = 3;
-    cout << "hamza" << endl;
+    Solution s = Solution();
+
+    vector<int> input = {1, 2, 3, 1};
+    bool output = s.containsDuplicate(input);
+    assert(output == true);
     return 0;
 }
